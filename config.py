b021f6d2fd6e72926860816ae767690c9a96cda9
marsggbo <marsggbo@users.noreply.github.com>
2026-04-07 05:06:42 +0000

    Deploying to gh-pages from @ marsggbo/marsggbo.github.io@fab1a751c9242896e13fd7153ac120d804dab613 🚀

--- config.py
+++
@@ -1,8 +0,0 @@
-# 配置 - Scholar ID
-SCHOLAR_URL = "https://scholar.google.com/citations?user=LYNKm_8AAAAJ"
-
-def get_scholar_id():
-    url = SCHOLAR_URL.strip()
-    if 'user=' in url:
-        return url.split('user=')[1].split('&')[0]
-    return url.strip()
